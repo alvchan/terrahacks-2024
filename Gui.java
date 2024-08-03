65d1b17057fe7e3840b4cf6e6ce298f042fc875a
James Boase <jamesboase123@gmail.com>
2024-08-03 12:08:52 -0400

    Created Gui boilerplate and text

--- Gui.java
+++ Gui.java
@@ -1,13 +1,10 @@
 import java.awt.*;      
 import java.awt.event.*; 
 import javax.swing.*;
-import java.util.*;
-import java.util.concurrent.Flow;
-import java.io.*;
 
-public class Gui extends JFrame implements ActionListener{
-   
-   
+
+public class Gui extends JFrame{
+
    public Gui()  
    {   
       JFrame frame = new JFrame("Arctic Tracker");
@@ -17,50 +14,13 @@ public class Gui extends JFrame implements ActionListener{
       int y = (screenSize.height - frameSize.height) / 4;
       frame.setLocation(x, y);
       frame.setSize(720, 960);
-      //frame.add(new Drawing());
-      
+      frame.add(new Drawing());
       frame.setResizable(false);
       frame.setVisible(true);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setLayout(new GridLayout(3, 1, 15, 15));
-      frame.getContentPane().setBackground(new Color(108,146,219));
-
+      frame.setLayout(null);
+      frame.getContentPane().setBackground(new Color(108,146,219)); 
       
-
-      //Title label
-      JLabel label1 = new JLabel("Arctic", SwingConstants.CENTER);
-      label1.setFont(new Font("Arial", Font.BOLD, 80));
-      label1.setVerticalAlignment(SwingConstants.BOTTOM);
-      JLabel label2 = new JLabel("Tracker", SwingConstants.CENTER);
-      label2.setFont(new Font("Arial", Font.BOLD, 80));
-      label2.setVerticalAlignment(SwingConstants.TOP);
-      //Stats button
-      JButton stats = new JButton("Stats");
-      stats.setForeground(new Color(85,145,169));
-      stats.setFocusPainted(false);
-      stats.setBackground(new Color(11,78,120));
-      stats.addActionListener(this);
-      stats.setActionCommand("stats");
-      stats.setFont(new Font("Arial", Font.PLAIN, 70));
-      //Ice cap
-      JButton icecap = new JButton("Ice Cap");
-      icecap.setForeground(new Color(85,145,169));
-      icecap.setFocusPainted(false);
-      icecap.setBackground(new Color(11,78,120));
-      icecap.addActionListener(this);
-      icecap.setActionCommand("icecap");
-      icecap.setFont(new Font("Arial", Font.PLAIN, 70));
-      JPanel buttons = new JPanel();
-      buttons.setLayout(new GridLayout(1,2));
-      buttons.setBackground(new Color(108,146,219));
-      buttons.add(stats);
-      buttons.add(icecap);
-      frame.add(label1);
-      frame.add(label2);
-      frame.add(buttons);
-      
-      
-    
    }   
       
    
@@ -70,16 +30,23 @@ public class Gui extends JFrame implements ActionListener{
       new Gui();     
    }
 
-   @Override
-   public void actionPerformed(ActionEvent e) {
-       // TODO Auto-generated method stub
-       if (e.getActionCommand().equals("stats")){
-         //make an instance of stats class here
-         System.out.println("test");
-       }
-       if (e.getActionCommand().equals("icecap")){
-         //make an instance of icecap class here
-         System.out.println("test2");
-       }
+   public class Drawing extends JComponent{ 
+
+      public Drawing(){}
+      
+      public void paint(Graphics g){ 
+         
+      
+         
+         g.fillOval(470, 320, 50, 50);
+         g.fillOval(510, 310, 50, 50);
+         
+         // title 
+         g.setColor(Color.BLACK);
+         Font largeSerifFont = new Font("Sans Serif", Font.BOLD, 60);
+         g.setFont(largeSerifFont);
+         g.drawString("ARCTIC", 230, 630);
+         g.drawString("TRACKER", 200, 680);
+      }
    }
 }
