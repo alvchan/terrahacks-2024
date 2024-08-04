8809581c422a59311bd8c7a8c4ed176cc57b2381
James Boase <jamesboase123@gmail.com>
2024-08-03 20:59:36 -0400

    grab tracker info for stats page

--- Stats.java
+++ Stats.java
@@ -4,30 +4,33 @@ import java.awt.* ;
 import java.awt.event.*;
 import javax.swing.*;
 
-import MainMenu.Drawing;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.*;
 
 public class Stats extends JFrame{
+    Tracker t1;
     public Stats(){
-        
+        t1 = new Tracker();
+        HashMap<String, Long> values = t1.value();
+        ArrayList<String> apps = new ArrayList<String>();
+        for (String s : values.keySet()){
+            apps.add(s);
+        }
         //Create labels
-        JLabel label1 = new JLabel("test1");
+        JLabel label1 = new JLabel("1. " + apps.get(0));
         label1.setFont(new Font("Arial", Font.PLAIN, 70));
-        JLabel label2 = new JLabel("test2");
+        JLabel label2 = new JLabel("2. " + apps.get(1));
         label2.setFont(new Font("Arial", Font.PLAIN, 70));
-        JLabel label3 = new JLabel("test3");
+        JLabel label3 = new JLabel("3. " + apps.get(2));
         label3.setFont(new Font("Arial", Font.PLAIN, 70));
-        JLabel label4 = new JLabel("test4");
+        JLabel label4 = new JLabel("4. " + apps.get(3));
         label4.setFont(new Font("Arial", Font.PLAIN, 70));
-        JLabel label5 = new JLabel("test5");
+        JLabel label5 = new JLabel("5. " + apps.get(4));
         label5.setFont(new Font("Arial", Font.PLAIN, 70));
         //Create Panel
         JPanel appList = new JPanel();
-        JPanel graph = new JPanel();
-
         appList.setBackground(new Color(108,146,219));
         appList.setLayout(new GridLayout(5, 1, 10, 10));
         appList.add(label1);
@@ -37,9 +40,28 @@ public class Stats extends JFrame{
         appList.add(label5);
         add(appList);
 
-        add(graph);
+        //Create other labels
+        JLabel label6 = new JLabel("" + values.get(apps.get(0)));
+        label1.setFont(new Font("Arial", Font.PLAIN, 70));
+        JLabel label7 = new JLabel("" + values.get(apps.get(1)));
+        label2.setFont(new Font("Arial", Font.PLAIN, 70));
+        JLabel label8 = new JLabel("" + values.get(apps.get(2)));
+        label3.setFont(new Font("Arial", Font.PLAIN, 70));
+        JLabel label9 = new JLabel("" + values.get(apps.get(3)));
+        label4.setFont(new Font("Arial", Font.PLAIN, 70));
+        JLabel label10 = new JLabel("" + values.get(apps.get(4)));
+        label5.setFont(new Font("Arial", Font.PLAIN, 70));
+        JPanel numList = new JPanel();
+        numList.setBackground(new Color(108,146,219));
+        numList.setLayout(new GridLayout(5, 1, 10, 10));
+        numList.add(label6);
+        numList.add(label7);
+        numList.add(label8);
+        numList.add(label9);
+        numList.add(label10);
+        add(numList);
     }
-    
+
     
 
 }
